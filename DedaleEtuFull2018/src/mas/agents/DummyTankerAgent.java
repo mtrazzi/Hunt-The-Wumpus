11721d9af38cfa80b15ca9f555f0cd0d34c35eaf
mtrazzi <mtrazzi@student.42.fr>
2018-04-25 15:46:45 +0200

    updated Tanker

--- DedaleEtuFull2018/src/mas/agents/DummyTankerAgent.java
+++ DedaleEtuFull2018/src/mas/agents/DummyTankerAgent.java
@@ -3,6 +3,7 @@ package mas.agents;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Stack;
 
 import env.Attribute;
@@ -16,6 +17,7 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import mas.abstractAgent;
 import mas.agents.DummyCollectorAgent.RandomWalkExchangeBehaviour;
+import mas.behaviours.GeneralSimpleBehaviour;
 import mas.graph.Graph;
 
 
@@ -92,30 +94,102 @@ public class DummyTankerAgent extends GeneralAgent{
  * 
  **************************************/
 
-class RandomTankerBehaviour extends TickerBehaviour{
-	/**
-	 * When an agent choose to move
-	 *  
-	 */
+class RandomTankerBehaviour extends GeneralSimpleBehaviour{
+
 	private static final long serialVersionUID = 9088209402507795289L;
 
-	public RandomTankerBehaviour (final mas.abstractAgent myagent) {
-		super(myagent, 10000);
-		//super(myagent);
+	public RandomTankerBehaviour(final mas.abstractAgent myagent) {
+		super(myagent);
 	}
 
-	@Override
-	public void onTick() {
-		//Example to retrieve the current position
-		String myPosition=((mas.abstractAgent)this.myAgent).getCurrentPosition();
+	public void action() {
+		// Set verbose to true do debug the behaviour
+		boolean verbose = true;
+		
+		//define agent = agent from the behavior casted to general agent
+		mas.agents.GeneralAgent agent = getGeneralAgent();
+		
+		if (verbose)
+			System.out.println(agent.getLocalName() + " -> STARTING BEHAVIOUR!");
+		
+		//Get current position
+		String myPosition = agent.getCurrentPosition();
 
-		if (myPosition!=""){
-			//List of observable from the agent's current position
-			List<Couple<String,List<Attribute>>> lobs=((mas.abstractAgent)this.myAgent).observe();//myPosition
+		// Prints the current position
+		System.out.println("Position of: " + agent.getLocalName() + ": " + myPosition);
+
+		if (myPosition != "") {
+			String myMove;
 			
-			//DO NOTHING. But it could be a good idea to change that
+			// List of observable from the agent's current position
+			List<Couple<String, List<Attribute>>> lobs = agent.observe();
+			if (verbose)
+				System.out.println(agent.getLocalName() + " -- list of observables: " + lobs);
+			
+			/////////////////////////////////
+			//// INTERBLOCKING
+			String lastMove = agent.getLastMove();
+			
+
+			
+			// WITH RANDOM
+			if (agent.getLastMove() != "" && !myPosition.equals(agent.getLastMove())) {
+				System.err.println("MOVE DIDN'T WORK");
+				while (!agent.getStack().empty())
+					agent.getStack().pop();
+				Random r = new Random();
+				Integer moveId=r.nextInt(lobs.size());
+				myMove = lobs.get(moveId).getLeft();
+			}
+			
+			/////////////////////////////////
+			//// NO INTERBLOCKING
+			else {
+				/////////////////////////////////
+				//// STEP 1) Update Graph
+				
+				// Add myPosition to Graph
+				agent.getGraph().addVertex(myPosition);
+	
+				// Set Node as discovered
+				agent.getHashmap().put(myPosition, "discovered");
+	
+				/////////////////////////////////
+				//// STEP 2) Update the Hashmap
+				agent.UpdateEdges(myPosition, lobs);
+				
+				/////////////////////////////////
+				//// STEP 3) Update the Stack if empty
+				
+				if (agent.getStack().empty())
+					agent.getGraph().bfs(myPosition, agent.getHashmap(),agent.getStack());
+	
+				/////////////////////////////////
+				//// STEP 4) Pick the next Move and do it
+				
+				// Pick the next Node to go
+				myMove = agent.getStack().pop();
+			}
+			
+			//Debug next move
+			if (verbose) {
+				System.out.println(agent.getLocalName() +"  -> Next Move: " + myMove);
+				System.out.print(agent.getLocalName() +"  ->");
+				getGeneralAgent().printStack();
+			}
+			
+			// Set last move to the next move, for next iteration
+			agent.setLastMove(myMove);
+			
+			// Move to the picked location (must be last action)
+			agent.moveTo(myMove);
 		}
 
 	}
 
+	public boolean done() {
+		this.sleep(100);
+
+		return (this.getGeneralAgent().areAllNodesVisited());
+	}
 }
